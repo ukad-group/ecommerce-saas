4ae4edd1e7198e41e4939ccb1cd93616099c88c7
AndrewMorgun <andrey.morgun@ukad-group.com>
2026-07-20 15:45:57 +0200

    feat(umbraco): remove per-product Attributes editor from product workspace
    Attributes are managed centrally in Commerce -> Attributes, so the workspace
    footer 'Attributes' button (Update Attributes panel) is removed from all three
    footer states. Global attributes were already read-only/non-detachable per
    product; the 'Custom properties' + Add field (ad-hoc per-product fields) stays.
    (The now-unreachable Update Attributes panel/handlers are left as dead code.)

--- umbraco/plugin/EComm.Umbraco.Commerce/wwwroot/components/workspaceViews/products-workspace-view.js
+++ umbraco/plugin/EComm.Umbraco.Commerce/wwwroot/components/workspaceViews/products-workspace-view.js
@@ -2797,9 +2797,6 @@ class ECommProductsWorkspaceView extends UmbElementMixin(LitElement) {
                 <uui-button look="secondary" color="positive" @click=${this.startAddVariant} ?disabled=${this.saving}>
                   Add Variant
                 </uui-button>
-                <uui-button look="secondary" @click=${this.startEditOptions} ?disabled=${this.saving}>
-                  Attributes
-                </uui-button>
                 <uui-button look="secondary" @click=${this.startManageOptions} ?disabled=${this.saving}>
                   Add-on Options (${(this.editedProduct?.options || []).length})
                 </uui-button>
@@ -3389,9 +3386,6 @@ class ECommProductsWorkspaceView extends UmbElementMixin(LitElement) {
                 Create product Variants
               </uui-button>
 
-              <uui-button look="secondary" @click=${this.startEditOptions} ?disabled=${this.saving}>
-                Attributes
-              </uui-button>
 
               <uui-button look="secondary" @click=${this.startManageOptions} ?disabled=${this.saving}>
                 Add-on Options (${(this.editedProduct?.options || []).length})
@@ -4119,9 +4113,6 @@ class ECommProductsWorkspaceView extends UmbElementMixin(LitElement) {
       <div class="button-group">
         <uui-button look="secondary" @click=${this.cancelEdit} ?disabled=${this.saving}>
           Close
-        </uui-button>
-        <uui-button look="secondary" @click=${this.startEditOptions} ?disabled=${this.saving}>
-          Attributes
         </uui-button>
         <uui-button look="secondary" @click=${this.startManageOptions} ?disabled=${this.saving}>
           Add-on Options (${(this.editedProduct?.options || []).length})
